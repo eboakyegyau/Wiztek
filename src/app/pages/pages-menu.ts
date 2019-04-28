61f762783f3ec51770bad73325bac1d2f63966a8
Emmanuel <gyab12@yahoo.com>
2019-04-28 14:03:51 +0200

    edited footer+ removed layout settings

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -2,24 +2,24 @@ import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
-    title: 'Dashboard',
+    title: 'Home',
     icon: 'nb-home',
     link: '/pages/dashboard',
     home: true,
   },
   {
     title: 'Services',
-    icon: 'nb-locked',
+    icon: 'nb-e-commerce',
     link: '/pages/dashboard',
   },
   {
     title: 'Free Consultation',
-    icon: 'nb-locked',
+    icon: 'nb-phone',
     link: '/pages/dashboard',
   },
   {
     title: 'About',
-    icon: 'nb-locked',
+    icon: 'nb-location',
     link: '/pages/dashboard',
   },
 ];
