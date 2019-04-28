121ba5ad4b1af71d542bff41cd6d2bc386407fd8
Emmanuel <gyab12@yahoo.com>
2019-04-28 21:59:47 +0200

    fixed tslint for github

--- src/app/pages/consultation/consultation.component.ts
+++ src/app/pages/consultation/consultation.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ngx-consultation',
   templateUrl: './consultation.component.html',
-  styleUrls: ['./consultation.component.scss']
+  styleUrls: ['./consultation.component.scss'],
 })
 export class ConsultationComponent implements OnInit {
 
